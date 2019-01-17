834ffa22b2b4a4fb83cc83cc590fe44d2e47547f
Supanut konngern <supanut.kon@gmail.com>
2019-01-17 14:11:24 +0700

    Updates 2

--- app/src/main/java/com/example/cv/aninterface/login.java
+++ app/src/main/java/com/example/cv/aninterface/login.java
@@ -2,6 +2,7 @@ package com.example.cv.aninterface;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -55,7 +56,7 @@ public class login extends AppCompatActivity
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(@androidx.annotation.NonNull CalendarView view, int year, int month, int dayOfMonth) {
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String date = (month + 1) + "/" + dayOfMonth + "/" + year;
                 myDate.setText(date);
             }
