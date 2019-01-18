eb12bcdb72aed405c5fcf5324e944c0846fbfd0c
Supanut konngern <supanut.kon@gmail.com>
2019-01-18 14:21:12 +0700

    Update bottmnavigation.java

--- app/src/main/java/com/example/cv/aninterface/bottomnavigation.java
+++ app/src/main/java/com/example/cv/aninterface/bottomnavigation.java
@@ -1,11 +1,14 @@
 package com.example.cv.aninterface;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class bottomnavigation extends AppCompatActivity {
@@ -24,7 +27,6 @@ public class bottomnavigation extends AppCompatActivity {
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_plan);
-
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -39,9 +41,14 @@ public class bottomnavigation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bottomnavigation);
 
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+
+
     }
 
 }
