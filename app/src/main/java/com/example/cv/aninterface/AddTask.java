1e2afad77cd5fa10678b6a285cc5179bc68c769d
iNonzq <nps.kaibutr@hotmail>
2019-01-22 19:42:11 +0700

    add task

--- app/src/main/java/com/example/cv/aninterface/AddTask.java
+++ app/src/main/java/com/example/cv/aninterface/AddTask.java
@@ -14,14 +14,20 @@ public class AddTask extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_task)
+        setContentView(R.layout.activity_task);
 
-        Spinner mySpinner = (Spinner) findViewById(R.id.inLocation);
+        Spinner mySpinner1 = (Spinner) findViewById(R.id.inLocation);
+        Spinner mySpinner2 = (Spinner) findViewById(R.id.ObjPerson);
 
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(AddTask.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.inLocation));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner.setAdapter(myAdapter);
+        ArrayAdapter<String> myAdapter1 = new ArrayAdapter<String>(AddTask.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.inLocation));
+        myAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner1.setAdapter(myAdapter1);
 
+        ArrayAdapter<String> myAdapter2 = new ArrayAdapter<String>(AddTask.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.ObjPerson));
+        myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner2.setAdapter(myAdapter2);
+
+        //header Navigation Bar
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
