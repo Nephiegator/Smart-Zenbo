0455cab6620a4dd56933abd9277386159cf4e831
Supanut konngern <supanut.kon@gmail.com>
2019-01-23 20:08:31 +0700

    update addplan button

--- app/src/main/java/com/example/cv/aninterface/MainListActivity.java
+++ app/src/main/java/com/example/cv/aninterface/MainListActivity.java
@@ -1,5 +1,6 @@
 package com.example.cv.aninterface;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -43,17 +44,12 @@ public class MainListActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                Intent addplan = new Intent(MainListActivity.this, AddPlan.class);
+                startActivity(addplan);
+                break;
+        } return false;
     }
 
     private List<BaseItem> createItem() {
