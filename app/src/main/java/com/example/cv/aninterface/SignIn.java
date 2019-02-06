212c2b5d51fecd2035b8d83c176eaa15f1e3a318
iNonzq <nps.kaibutr@hotmail>
2019-02-06 18:45:51 +0700

    Update SignIn.java

--- app/src/main/java/com/example/cv/aninterface/SignIn.java
+++ app/src/main/java/com/example/cv/aninterface/SignIn.java
@@ -1,52 +1,37 @@
 package com.example.cv.aninterface;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageButton;
-import android.content.Intent;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
-import android.support.v7.widget.Toolbar;
-import android.widget.ImageButton;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 
 public class SignIn extends AppCompatActivity {
 
-    private static final String LOGIN_KEY = "LOGIN_KEY";
+    private CallbackManager mCallbackManager;
+    private static final String TAG = "FACELOG";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-        /*SharedPreferences pref = getPreferences(Context.MODE_PRIVATE);
-        if (pref.getBoolean(LOGIN_KEY, false)) {
-            startActivity(new Intent(this, Home.class));
-            finish();
-        } else {
-            pref.edit().putBoolean(LOGIN_KEY, true).apply();
-        }
-
-        printKeyHash(); */
-
         // Initialize Facebook Login button
         mCallbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = findViewById(R.id.buttonFacebookLogin);
+        LoginButton loginButton = findViewById(R.id.login_btn);
         loginButton.setReadPermissions("email", "public_profile");
         loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.d(TAG, "facebook:onSuccess:" + loginResult);
-                handleFacebookAccessToken(loginResult.getAccessToken());
+                //handleFacebookAccessToken(loginResult.getAccessToken());
             }
 
             @Override
@@ -63,18 +48,12 @@ public class SignIn extends AppCompatActivity {
         });
     }
 
-    /*private void printKeyHash() {
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo("com.example.cv.aninterface", PackageManager.GET_SIGNATURES);
-            for (Signature signature:info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-    }*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
