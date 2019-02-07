432f5fcc33a56dac8329b587dd51a3fd963e0bb6
iNonzq <nps.kaibutr@hotmail>
2019-02-07 15:47:03 +0700

    nav

--- app/src/main/java/com/example/cv/aninterface/SignIn.java
+++ app/src/main/java/com/example/cv/aninterface/SignIn.java
@@ -1,59 +1,141 @@
 package com.example.cv.aninterface;
 
-
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
+public class SignIn extends AppCompatActivity implements View.OnClickListener {
 
-
-public class SignIn extends AppCompatActivity {
-
-    private CallbackManager mCallbackManager;
-    private static final String TAG = "FACELOG";
+    private CallbackManager callbackManager;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    //private TextView txtUser;
+    private TextView txtEmail;
+    //private ImageView imgProfile;
+    //private LoginButton logoutButton;
+    private LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        //txtUser = (TextView) findViewById(R.id.txtUser);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
+        //imgProfile = (ImageView) findViewById(R.id.imgProfile);
+        firebaseAuth = FirebaseAuth.getInstance();
+        callbackManager = CallbackManager.Factory.create();
 
-        // Initialize Facebook Login button
-        mCallbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = findViewById(R.id.login_btn);
-        loginButton.setReadPermissions("email", "public_profile");
-        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+        loginButton = (LoginButton) findViewById(R.id.login_fb_btn);
+        //logoutButton.setOnClickListener(this);
+
+
+        loginButton.setReadPermissions("email", "public_profile");//user_status, publish_actions..
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Log.d(TAG, "facebook:onSuccess:" + loginResult);
-                //handleFacebookAccessToken(loginResult.getAccessToken());
+                handleFacebookAccessToken(loginResult.getAccessToken());
             }
 
             @Override
             public void onCancel() {
-                Log.d(TAG, "facebook:onCancel");
-                // ...
+                Toast.makeText(SignIn.this, "Sign In canceled", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onError(FacebookException error) {
-                Log.d(TAG, "facebook:onError", error);
-                // ...
+                Toast.makeText(SignIn.this, "Something bad happened", Toast.LENGTH_SHORT).show();
             }
         });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (AccessToken.getCurrentAccessToken() != null)
+                    Toast.makeText(SignIn.this, AccessToken.getCurrentAccessToken().getExpires().toString(), Toast.LENGTH_SHORT).show();
+                if (user != null) {
+                    String email = user.getEmail();
+                    String userName = user.getDisplayName();
+                    txtEmail.setText(email);
+                    //txtUser.setText(userName);
+                    Picasso.with(SignIn.this).load(user.getPhotoUrl()).into(imgProfile);
+                    loginButton.setVisibility(View.GONE);
+                    //logoutButton.setVisibility(View.VISIBLE);
+                } else {
+                    Log.d("TG", "SIGNED OUT");
+                    txtEmail.setText("");
+                    //txtUser.setText("");
+                    //imgProfile.setImageBitmap(null);
+                    loginButton.setVisibility(View.VISIBLE);
+                    //logoutButton.setVisibility(View.GONE);
+                }
+            }
+        };
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Pass the activity result back to the Facebook SDK
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    public void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(mAuthListener);
     }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignIn.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,
+                resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+        firebaseAuth.signOut();
+        LoginManager.getInstance().logOut();
+
+    }
 }
---
+++ app/src/main/java/com/example/cv/aninterface/login_info.java
@@ -0,0 +1,15 @@
+package com.example.cv.aninterface;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class login_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_header_login);
+
+
+    }
+}
