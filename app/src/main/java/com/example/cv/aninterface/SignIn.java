a1e4c708bf59773dec273adb53a1528ec32e1783
Supanut konngern <supanut.kon@gmail.com>
2019-02-14 21:18:34 +0700

    login with email

--- app/src/main/java/com/example/cv/aninterface/SignIn.java
+++ app/src/main/java/com/example/cv/aninterface/SignIn.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,12 +31,15 @@ import com.squareup.picasso.Picasso;
  public class SignIn extends AppCompatActivity {
 
     private CallbackManager callbackManager;
-    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth firebaseAuth, mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     //private TextView txtUser;
     private TextView txtEmail;
     //private ImageView imgProfile;
     private LoginButton loginButton;
+    private EditText iEmail,iPassword;
+    private Button Loginbtn;
+    private static final String TAG = "SignIn";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -136,5 +141,82 @@ import com.squareup.picasso.Picasso;
         firebaseAuth.signOut();
         LoginManager.getInstance().logOut();  */
 
+        iEmail = (EditText) findViewById(R.id.userlogin);
+        iPassword = (EditText) findViewById(R.id.password);
+        Loginbtn = (Button) findViewById(R.id.login);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+                }
+                // ...
+            }
+        };
+
+        Loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = iEmail.getText().toString();
+                String pass = iPassword.getText().toString();
+                if(!email.equals("") && !pass.equals("")){
+                    mAuth.signInWithEmailAndPassword(email,pass);
+                }else{
+                    toastMessage("You didn't fill in all the fields.");
+                }
+            }
+        });
+
+        /*btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                toastMessage("Signing Out...");
+            }
+        });
+
+        btnAddItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Switching Activities.");
+                Intent intent = new Intent(MainActivity.this, AddItemsToDatabase.class);
+                startActivity(intent);
+            }
+        });*/
     }
-}
+
+     @Override
+     public void onStart() {
+         super.onStart();
+         mAuth.addAuthStateListener(mAuthListener);
+     }
+
+     @Override
+     public void onStop() {
+         super.onStop();
+         if (mAuthListener != null) {
+             mAuth.removeAuthStateListener(mAuthListener);
+         }
+     }
+
+     //add a toast to show when successfully signed in
+     /**
+      * customizable toast
+      * @param message
+      */
+     private void toastMessage(String message){
+         Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+     }
+ }
+
