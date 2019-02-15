e6fdd51961b564480233633676a64d63a10e5553
Supanut konngern <supanut.kon@gmail.com>
2019-02-15 23:03:41 +0700

    complete and usable registration

--- app/src/main/java/com/example/cv/aninterface/register.java
+++ app/src/main/java/com/example/cv/aninterface/register.java
@@ -1,13 +1,31 @@
 package com.example.cv.aninterface;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class register extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class register extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth firebaseAuth;
+    private Button buttonSignup;
+    private EditText emailSignup;
+    private EditText passwordSignup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,17 +34,48 @@ public class register extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+        buttonSignup = (Button) findViewById(R.id.signup_btn);
+        emailSignup = (EditText) findViewById(R.id.signup_email);
+        passwordSignup = (EditText) findViewById(R.id.signup_password);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        buttonSignup.setOnClickListener(this);
 
 
+    }
 
+    private void registerUser() {
+
+        String email = emailSignup.getText().toString().trim();
+        String pass = passwordSignup.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter your email", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(pass)) {
+            Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(register.this, "Successfully registered", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(register.this, "Error occurred, please try again.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        registerUser();
     }
 
 }
