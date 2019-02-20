64378b4c88b12939e176f25c38247a9df79fc9c1
Supanut konngern <supanut.kon@gmail.com>
2019-02-20 13:31:38 +0700

    add task toolbar added

--- app/src/main/java/com/example/cv/aninterface/AddTask.java
+++ app/src/main/java/com/example/cv/aninterface/AddTask.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -18,7 +20,7 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -83,11 +85,33 @@ public class AddTask extends AppCompatActivity implements AdapterView.OnItemSele
 
 
         //header Navigation Bar
-        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.appbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
 
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainplan_menu, menu);
+        return true;
+    }
+
+
 
     public class State1 {
         public String loc = "";
@@ -126,6 +150,7 @@ public class AddTask extends AppCompatActivity implements AdapterView.OnItemSele
                 break;
         }
     }
+
 
 
     public void onNothingSelected(AdapterView<?> parent) {
--- app/src/main/java/com/example/cv/aninterface/MainListActivity.java
+++ app/src/main/java/com/example/cv/aninterface/MainListActivity.java
@@ -35,7 +35,7 @@ public class MainListActivity extends AppCompatActivity {
         adapter.setItemList(createItem());
         recyclerView.setAdapter(adapter);
 
-        //header Navigation Bar
+       /* //header Navigation Bar
         Toolbar toolbar = findViewById(R.id.appbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -46,25 +46,15 @@ public class MainListActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),Home.class));
             }
-        });
+        });*/
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.mainplan_menu, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
 
-        MenuItem z1 = (MenuItem) findViewById(R.id.action_add);
-        Intent Z1 = new Intent(MainListActivity.this, AddPlan.class);
-        startActivity(Z1);
 
-        return false;
-    }
+
+
 
     private List<BaseItem> createItem() {
         List<BaseItem> itemList = new ArrayList<>();
