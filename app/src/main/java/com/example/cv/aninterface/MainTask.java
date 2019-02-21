3b78264dd1195b8275d452cdb68635d55e34d0b4
Supanut konngern <supanut.kon@gmail.com>
2019-02-21 14:51:01 +0700

    add bottomnavigation

--- app/src/main/java/com/example/cv/aninterface/MainTask.java
+++ app/src/main/java/com/example/cv/aninterface/MainTask.java
@@ -2,9 +2,13 @@ package com.example.cv.aninterface;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.CollectionReference;
@@ -25,6 +29,31 @@ public class MainTask extends AppCompatActivity {
         setContentView(R.layout.activity_maintask);
 
         setUpRecyclerView();
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        NavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_home:
+                        Intent a1 = new Intent(Home.this, Home.class);
+                        startActivity(a1);
+                        break;
+                    case R.id.navigation_task:
+                        Intent a2 = new Intent(Home.this, MainTask.class);
+                        startActivity(a2);
+                        break;
+                    case R.id.navigation_plan:
+                        Intent a3 = new Intent(Home.this, MainListActivity.class);
+                        startActivity(a3);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     private void setUpRecyclerView() {
@@ -53,4 +82,6 @@ public class MainTask extends AppCompatActivity {
         super.onStop();
         adapter.stopListening();
     }
+
+
 }
