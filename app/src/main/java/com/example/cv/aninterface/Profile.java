2e40336f38e747acfdfe3a84d0277862a12a4132
Supanut konngern <supanut.kon@gmail.com>
2019-04-03 21:40:19 +0700

    Update Profile.java

--- app/src/main/java/com/example/cv/aninterface/Profile.java
+++ app/src/main/java/com/example/cv/aninterface/Profile.java
@@ -5,7 +5,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,7 +21,7 @@ public class Profile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.profile);
 
         mAuth = FirebaseAuth.getInstance();
 
