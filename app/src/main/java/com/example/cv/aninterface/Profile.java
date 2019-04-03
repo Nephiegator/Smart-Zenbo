e9f8e492ab3f234b5b15baaed317623457cb3cb3
Supanut konngern <supanut.kon@gmail.com>
2019-04-03 13:15:05 +0700

    Update

--- app/src/main/java/com/example/cv/aninterface/Profile.java
+++ app/src/main/java/com/example/cv/aninterface/Profile.java
@@ -3,7 +3,6 @@ package com.example.cv.aninterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatCheckBox;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
