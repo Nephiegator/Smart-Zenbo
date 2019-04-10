646996f55bbe19a5dbcefcfee8c30e3fad440ac3
stupidcubid <48797350+stupidcubid@users.noreply.github.com>
2019-04-11 00:45:07 +0700

    update AddTaskMed

--- app/src/main/java/com/example/cv/aninterface/AddTaskMed.java
+++ app/src/main/java/com/example/cv/aninterface/AddTaskMed.java
@@ -1,27 +1,29 @@
 package com.example.cv.aninterface;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class AddTaskMed extends AppCompatActivity  {
+public class AddTaskMed extends AppCompatActivity implements View.OnClickListener {
 
     private TextInputEditText txt_title;
     private TextInputEditText txt_description;
-    RadioGroup r1, r2;
-    RadioButton bf, at;
-    private String sBefore, sAfter;
-    private String one_tab, two_tab, thee_tab;
-    private String yy, xx;
+    private String yy, xx, per, med;
 
     private FirebaseFirestore db;
 
@@ -38,7 +40,36 @@ public class AddTaskMed extends AppCompatActivity  {
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button Create = (Button) findViewById(R.id.create_btn);
+        Create.setOnClickListener(this);
+        //Spinner period
+        Spinner spperiod = (Spinner) findViewById(R.id.period);
 
+        ArrayAdapter<AddTaskMed.State3> spinnerArrayAdapter3 = new ArrayAdapter<AddTaskMed.State3> (this,
+                android.R.layout.simple_spinner_item, new AddTaskMed.State3[] {
+                new AddTaskMed.State3("None"),
+                new AddTaskMed.State3("Before food"),
+                new AddTaskMed.State3("After food")
+        });
+        spperiod.setPrompt("Period");
+        spperiod.setAdapter(spinnerArrayAdapter3);
+        spperiod.setOnItemSelectedListener(new AddTaskMed.MyOnItemSelectedListener());
+
+        //Spinner pills
+        Spinner sppill = (Spinner) findViewById(R.id.pill);
+        ArrayAdapter<AddTaskMed.State4> spinnerArrayAdapter4 = new ArrayAdapter<State4> (this,
+                android.R.layout.simple_spinner_item, new AddTaskMed.State4[] {
+                new AddTaskMed.State4("None"),
+                new AddTaskMed.State4("1 pill"),
+                new AddTaskMed.State4("2 pills"),
+                new AddTaskMed.State4("3 pills"),
+                new AddTaskMed.State4("4 pills")
+        });
+        sppill.setPrompt("Amount of pill");
+        sppill.setAdapter(spinnerArrayAdapter4);
+        sppill.setOnItemSelectedListener(new AddTaskMed.MyOnItemSelectedListener());
+
+        //Spinner location and object person
         Spinner splocation = (Spinner) findViewById(R.id.inLocation);
         Spinner spperson = (Spinner) findViewById(R.id.ObjPerson);
 
@@ -47,7 +78,7 @@ public class AddTaskMed extends AppCompatActivity  {
                 android.R.layout.simple_spinner_item, new AddTaskMed.State1[] {
                 new AddTaskMed.State1("None"),
                 new AddTaskMed.State1("Bedroom"),
-                new AddTaskMed.State1("Kitchen"),
+                new AddTaskMed.State1("Kitchen")
         });
         splocation.setAdapter(spinnerArrayAdapter1);
         splocation.setOnItemSelectedListener(new AddTaskMed.MyOnItemSelectedListener());
@@ -65,7 +96,7 @@ public class AddTaskMed extends AppCompatActivity  {
 
     }
 
-    private boolean validateInputs(String title, String description, String location, String person){
+    private boolean validateInputs(String title, String description, String location, String person,String period, String pill){
         if (title.isEmpty()){
             txt_title.setError("Title Required");
             txt_title.requestFocus();
@@ -78,11 +109,15 @@ public class AddTaskMed extends AppCompatActivity  {
             return true;
         }
         if (location.isEmpty()){
-
-
             return false;
         }
         if (person.isEmpty()){
+            return false;
+        }
+        if (period.isEmpty()) {
+            return false;
+        }
+        if (pill.isEmpty()) {
             return false;
         }
 
@@ -114,6 +149,30 @@ public class AddTaskMed extends AppCompatActivity  {
         }
     }
 
+    public class State3 {
+        public String period = "";
+
+        public State3(String _period){
+            period = _period;
+        }
+
+        public String toString() {
+            return period;
+        }
+    }
+
+    public class State4 {
+        public String pill = "";
+
+        public State4(String _pill){
+            pill = _pill;
+        }
+
+        public String toString() {
+            return pill;
+        }
+    }
+
     public class MyOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
 
         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -123,6 +182,12 @@ public class AddTaskMed extends AppCompatActivity  {
                     break;
                 case R.id.ObjPerson:
                     yy = parent.getItemAtPosition(position).toString();
+                    break;
+                case R.id.period:
+                    per = parent.getItemAtPosition(position).toString();
+                    break;
+                case R.id.pill:
+                    med = parent.getItemAtPosition(position).toString();
                     break;
             }
 
@@ -134,12 +199,51 @@ public class AddTaskMed extends AppCompatActivity  {
         }
     }
 
-    public void creatTask() {
+    public void createTask() {
         String title = txt_title.getText().toString().trim();
         String description = txt_description.getText().toString().trim();
         String location = xx;
         String person = yy;
-        String period = sBefore;
+        String period = per;
+        String pill = med;
+
+        if (!validateInputs(title, description, location, person, period, pill)) {
+            CollectionReference dbTaskMed = db.collection("Medication Task");
+
+            dbTaskMed tm = new dbTaskMed(
+                    title,
+                    description,
+                    location,
+                    person,
+                    period,
+                    pill
+            );
+
+            dbTaskMed.add(tm)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Toast.makeText(AddTaskMed.this, "Created Successfully", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(AddTaskMed.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.create_btn:
+                createTask();
+                finish();
+                break;
+
+        }
     }
 
 }
