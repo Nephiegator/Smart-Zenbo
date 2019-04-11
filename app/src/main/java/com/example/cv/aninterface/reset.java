7d3755497c2e1a59822a19530b2d963244a9adac
Supanut konngern <supanut.kon@gmail.com>
2019-04-12 00:23:35 +0700

    Update reset.java

--- app/src/main/java/com/example/cv/aninterface/reset.java
+++ app/src/main/java/com/example/cv/aninterface/reset.java
@@ -1,4 +1,52 @@
 package com.example.cv.aninterface;
 
-public class reset {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.net.PasswordAuthentication;
+
+public class reset extends AppCompatActivity {
+
+    private TextInputEditText email_reset;
+    private Button button_reset;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resetpass);
+
+        email_reset = findViewById(R.id.email_reset);
+        button_reset = findViewById(R.id.button_reset);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        button_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.sendPasswordResetEmail(email_reset.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(reset.this,
+                                    "Password send to your email", Toast.LENGTH_LONG).show();
+                        }else {
+                            Toast.makeText(reset.this,
+                                    task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
 }
